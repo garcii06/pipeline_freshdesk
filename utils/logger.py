2f40540cf6479ceccad61819389a654c4904699b
Israel García <42307512+garcii06@users.noreply.github.com>
2026-04-26 03:59:15 +0000

    Added logger util module.

---
+++ utils/logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        
+        os.makedirs('logs', exist_ok=True)
+        log_filename = f"logs/pipeline_{datetime.now().strftime('%Y%m%d')}.log"
+        file_handler = logging.FileHandler(log_filename)
+        file_handler.setFormatter(formatter)
+        
+        logger.addHandler(console_handler)
+        logger.addHandler(file_handler)
+    
+    return logger
